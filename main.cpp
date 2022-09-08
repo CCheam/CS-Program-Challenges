da6d02052d05e1590246242912e7d6d8c3e9d9aa
Calvin Cheam <88858058+CCheam@users.noreply.github.com>
2022-09-08 18:43:15 +0000

    start problem 13

--- main.cpp
+++ main.cpp
@@ -1,24 +1,8 @@
 #include <iostream>
 #include <iomanip>
 int main() {
-  int seconds, minutes, hours, days;
-  std::cout<<"Enter a number of seconds"<<std::endl;
-  std::cin >> seconds;
-  minutes=seconds/60;
-  hours=seconds/3600;
-  days=seconds/86400;
-  if(seconds>=60){
-    if(seconds>=3600){
-      if(seconds>=86400){
-        std::cout<<"In "<<seconds<<" seconds there are approximately "<<days<<" days"<<std::endl;
-    return 0;
-      }
-      std::cout<<"In "<<seconds<<" seconds there are approximately"<<hours<<" hours"<<std::endl;
-    return 0;
-    }
-    std::cout<<"In "<<seconds<<" seconds there are approximately "<<minutes<<" minutes"<<std::endl;
-    return 0;
-  }
-  std::cout<<seconds<<" seconds is too small to be any other unit of time"<<std::endl;
+  int weight, distance;
+  double rate;
+  std::cout<<"Weight of package: "<<distance<<std::endl;
     return 0;
 }
