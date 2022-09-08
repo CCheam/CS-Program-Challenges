0244a8b23ce090da4dcda672da369c20b199e352
Calvin Cheam <88858058+CCheam@users.noreply.github.com>
2022-09-08 18:38:51 +0000

    Problem 7 Completed

--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <iomanip>
 int main() {
-  int month,day,year,mult;
-  std::cout<<"Enter a date in form M/D/Y"<<std::endl;
-  std::cin >> month >> day >> year;
-  mult=month*day;
-if (mult == year){
-  std::cout <<"This date is magical."<<std::endl;
-}
-  else{
-    std::cout <<"This date isn't magical."<<std::endl;
+  int seconds, minutes, hours, days;
+  std::cout<<"Enter a number of seconds"<<std::endl;
+  std::cin >> seconds;
+  minutes=seconds/60;
+  hours=seconds/3600;
+  days=seconds/86400;
+  if(seconds>=60){
+    if(seconds>=3600){
+      if(seconds>=86400){
+        std::cout<<"In "<<seconds<<" seconds there are approximately "<<days<<" days"<<std::endl;
+    return 0;
+      }
+      std::cout<<"In "<<seconds<<" seconds there are approximately"<<hours<<" hours"<<std::endl;
+    return 0;
+    }
+    std::cout<<"In "<<seconds<<" seconds there are approximately "<<minutes<<" minutes"<<std::endl;
+    return 0;
   }
+  std::cout<<seconds<<" seconds is too small to be any other unit of time"<<std::endl;
     return 0;
 }
