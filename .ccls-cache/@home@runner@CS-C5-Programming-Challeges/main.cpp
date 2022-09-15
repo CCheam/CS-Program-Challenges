8c52100f7ee52505dd404abe3247a62296475dba
Calvin Cheam <88858058+CCheam@users.noreply.github.com>
2022-09-15 18:55:04 +0000

    C5 Problem 4 Completed

--- .ccls-cache/@home@runner@CS-C5-Programming-Challeges/main.cpp
+++ .ccls-cache/@home@runner@CS-C5-Programming-Challeges/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 #include <iomanip>
 int main() {
+  int year =1;
+  double rise =1.5;
+  std::cout<<"Table of amount of ocean rise assuming 1.5mm yearly rise"<<std::endl;
+  while(year<=25){
+    double amt=rise*year;
+    std::cout<<"Year: "<<year<<"   Rise Amount: "<<amt<<"mm"<<std::endl;
+    year++;
+  }
  
+  return 0;
 }
