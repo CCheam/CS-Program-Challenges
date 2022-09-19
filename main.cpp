efdf9e2cf6c4c3fb6a1413ddfb0c69d4bd77e4ad
Calvin Cheam <88858058+CCheam@users.noreply.github.com>
2022-09-19 17:58:30 +0000

    Actual completion of 4

--- main.cpp
+++ main.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
 #include <iomanip>
 int main() {
-  int num=1;
-  int years,months,total, mRain;
-  double avg;
-  std::cout<<"Input # of years: "<<std::endl;
-  std:cin>>years;
-  while(num<=years){
-    for(int i=0;i<12,i++) {
-      std::cout<<"Input amount of rainfall for Month "<<i": "<<std::endl;
-      std::cin>>mRain;
-    total+=mRain;
-      months++;
-    }
-    num++;
+ double calPMin=3.9;
+  double calories;
+  for(int i=10;i<=30;i+=5){
+    calories=calPMin*i;
+std::cout<<"Calories burned at "<<i<<" minutes: "<<calories<<std::endl;
   }
-avg=total/months;
-std::cout<<"Total Months: "<<months<<std::endl;
-std::cout<<"Total Rainfall: "<<total<<std::endl;
-std::cout<<"Avg Rainfall: "<<avg<<" inches"std::endl;
-  return 0;
 }
