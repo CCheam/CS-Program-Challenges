b404e073de4fedb67af06ac214a440bb9c8add25
Calvin Cheam <88858058+CCheam@users.noreply.github.com>
2022-09-19 18:21:52 +0000

    completed 22

--- main.cpp
+++ main.cpp
@@ -1,26 +1,14 @@
 #include <iostream>
 #include <iomanip>
 int main() {
-  int num=1;
-  int years,months,total;
-  double avg;
-  total=0;
-  months=0;
-  std::cout<<"Input # of years: "<<std::endl;
-  std::cin>>years;
-  while(num<=years){
-    for(int i=0;i<12;i++) {
-      int mRain;
-      std::cout<<"Input amount of rainfall for Month "<<i+1<<": "<<std::endl;
-      std::cin>>mRain;
-    total+=mRain;
-      months++;
-    }
-    num++;
+  int inp;
+  std::cout<<"Input a #:"<<std::endl;
+  std::cin>>inp;
+for(int i =0;i<inp;i++){
+  for(int j=0;j<inp;j++){
+    std::cout<<"x";
   }
-avg=total/months;
-std::cout<<"Total Months: "<<months<<std::endl;
-std::cout<<"Total Rainfall: "<<total<<" inches"<<std::endl;
-std::cout<<"Avg Rainfall: "<<avg<<" inches"<<std::endl;
+  std::cout<<std::endl;
+}
   return 0;
   }
