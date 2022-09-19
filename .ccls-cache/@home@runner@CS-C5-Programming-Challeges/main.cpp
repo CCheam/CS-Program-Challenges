35e69e3e505b977f802763d7a6c5e23cd9564a7c
Calvin Cheam <88858058+CCheam@users.noreply.github.com>
2022-09-19 18:17:48 +0000

    completion of 10

--- .ccls-cache/@home@runner@CS-C5-Programming-Challeges/main.cpp
+++ .ccls-cache/@home@runner@CS-C5-Programming-Challeges/main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <iomanip>
 int main() {
- double calPMin=3.9;
-  double calories;
-  for(int i=10;i<=30;i+=5){
-    calories=calPMin*i;
-std::cout<<"Calories burned at "<<i<<" minutes: "<<calories<<std::endl;
+  int num=1;
+  int years,months,total;
+  double avg;
+  total=0;
+  months=0;
+  std::cout<<"Input # of years: "<<std::endl;
+  std::cin>>years;
+  while(num<=years){
+    for(int i=0;i<12;i++) {
+      int mRain;
+      std::cout<<"Input amount of rainfall for Month "<<i+1<<": "<<std::endl;
+      std::cin>>mRain;
+    total+=mRain;
+      months++;
+    }
+    num++;
   }
-}
+avg=total/months;
+std::cout<<"Total Months: "<<months<<std::endl;
+std::cout<<"Total Rainfall: "<<total<<" inches"<<std::endl;
+std::cout<<"Avg Rainfall: "<<avg<<" inches"<<std::endl;
+  return 0;
+  }
