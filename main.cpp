85789d70d407fc626def5a5c2aa4123c7ba1e1b3
Calvin Cheam <88858058+CCheam@users.noreply.github.com>
2022-09-22 19:10:39 +0000

    some progress on 24

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 using namespace std;
 int main() {
+  fstream c;
   int numNum,sum=0;
+  while{
+    numNum++;
+  }
+  c.open("random.txt",ios::in);
   return 0;
   }
