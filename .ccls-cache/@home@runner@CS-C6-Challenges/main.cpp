f4441a73e645365a677e3110088d8a209b66d285
Calvin Cheam <88858058+CCheam@users.noreply.github.com>
2022-09-26 18:34:05 +0000

    progress on 9

--- .ccls-cache/@home@runner@CS-C6-Challenges/main.cpp
+++ .ccls-cache/@home@runner@CS-C6-Challenges/main.cpp
@@ -2,7 +2,26 @@
 #include <iostream>
 
 using namespace std;
-int main() {
 
+double PvalueFunc(double &,double &, double &);
+int main() {
+  int years;
+  double interest,fValue;
+//input values
+cout<<"Input future value that you want: "<<endl;
+cin>>fValue;
+cout<<"Input interest rate: "<<endl;
+cin>>interest;
+cout<<"Input # of years money will collect interest for:"<<endl;
+cin>>years;
+  //function
+  
+  //output
+  cout<<;
   return 0;
+}
+
+double PvalueFunc(double &,double &, double &){
+  double PValue;
+  return PValue;
 }
