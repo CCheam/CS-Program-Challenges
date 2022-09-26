6d434644fbcd96b4cfdc7c6d4404547ec1231b32
Calvin Cheam <88858058+CCheam@users.noreply.github.com>
2022-09-26 18:43:30 +0000

    completed 9

--- .ccls-cache/@home@runner@CS-C6-Challenges/main.cpp
+++ .ccls-cache/@home@runner@CS-C6-Challenges/main.cpp
@@ -1,27 +1,30 @@
 #include <iomanip>
 #include <iostream>
-
+#include <math.h>
 using namespace std;
 
-double PvalueFunc(double &,double &, double &);
+double PValueFunc(double &fVal,double &inr, int &y){
+  double PValue;
+  double q;
+q=pow(1+inr,y);
+  PValue=fVal/q;
+  return PValue;
+}
+
 int main() {
   int years;
-  double interest,fValue;
+  double interest,fValue,pValue;
 //input values
 cout<<"Input future value that you want: "<<endl;
 cin>>fValue;
-cout<<"Input interest rate: "<<endl;
+cout<<"Input interest rate as a decimal: "<<endl;
 cin>>interest;
 cout<<"Input # of years money will collect interest for:"<<endl;
 cin>>years;
   //function
-  
+  pValue=PValueFunc(fValue,interest,years);
   //output
-  cout<<;
+  cout<<"You need to deposit $"<<pValue<<" to have $"<<fValue<<" in "<<years<<" years";
   return 0;
 }
 
-double PvalueFunc(double &,double &, double &){
-  double PValue;
-  return PValue;
-}
