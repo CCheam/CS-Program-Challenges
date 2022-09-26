6026b9ff2ad1bc3f6d3fbd5f533ebff112e61662
Calvin Cheam <88858058+CCheam@users.noreply.github.com>
2022-09-26 19:06:52 +0000

    base for 8

--- .ccls-cache/@home@runner@CS-C6-Challenges/main.cpp
+++ .ccls-cache/@home@runner@CS-C6-Challenges/main.cpp
@@ -1,53 +1,11 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
-// Write the prototypes for the getLength,
-// getWidth, getArea, and displayData
-// functions here.
-double getLength(double & l){
-  cout<<"Input length: ";
-  cin>>l;
-  return l;
-}
-
-double getWidth(double & w){
-  cout<<"Input width: ";
-  cin>>w;
-  return w;
-}
-
-double getArea(double &l,double&w,double &a){
-  a=l*w;
-  return a;
-}
-
-void displayData(double &l,double &w,double &a){
-  cout<<"Length: "<<l<<endl;
-  cout<<"Width: "<<w<<endl;
-  cout<<"Area: "<<a<<endl;
-}
 int main()
 {
-   double length,    // The rectangle's length
-          width,     // The rectangle's width
-          area;      // The rectangle's area
-          
-   // Get the rectangle's length.
-   getLength(length);
-   
-   // Get the rectangle's width.
-   getWidth(width);
-   
-   // Get the rectangle's area.
-   getArea(length,width,area);
-   
-   // Display the rectangle's data.
-   displayData(length, width, area);
-          
+ int coion
    return 0;
 }
 
-//***************************************************
-// You must write the getLength, getWidth, getArea, *
-// and displayData functions.                       *
 
