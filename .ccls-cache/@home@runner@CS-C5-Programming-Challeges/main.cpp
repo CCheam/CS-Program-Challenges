6f8387374c0a63d478edada0cb97b88ece50e1b2
Calvin Cheam <88858058+CCheam@users.noreply.github.com>
2022-09-26 18:17:40 +0000

    finished 24

--- .ccls-cache/@home@runner@CS-C5-Programming-Challeges/main.cpp
+++ .ccls-cache/@home@runner@CS-C5-Programming-Challeges/main.cpp
@@ -3,11 +3,27 @@
 #include <fstream>
 using namespace std;
 int main() {
-  fstream c;
-  int numNum,sum=0;
-  while{
+  ifstream c;
+  ofstream oFile;
+  int sum=0;
+  int i,avg;
+  double numNum;
+  c.open("Random.txt");
+  while(c>>i){
+    sum+=i;
     numNum++;
   }
-  c.open("random.txt",ios::in);
+  c.close();
+  avg=sum/(numNum);
+  //write to file
+  oFile.open("Output.txt");
+  oFile<<"Sum: "<<sum<<endl;
+  oFile<<"Total #s read: "<<numNum<<endl;
+  oFile<<"Average: "<<avg<<endl;
+  oFile.close();
+  //output
+  cout<<"Sum: "<<sum<<endl;
+  cout<<"Total #s read: "<<numNum<<endl;
+  cout<<"Average: "<<avg;
   return 0;
   }
