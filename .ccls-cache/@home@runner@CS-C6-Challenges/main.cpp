61c8e82971fc779869c7f832d0ef12474103a992
Calvin Cheam <88858058+CCheam@users.noreply.github.com>
2022-09-28 18:01:31 +0000

    progress on 8

--- .ccls-cache/@home@runner@CS-C6-Challenges/main.cpp
+++ .ccls-cache/@home@runner@CS-C6-Challenges/main.cpp
@@ -2,9 +2,32 @@
 #include <cstdlib>
 using namespace std;
 
+int coinToss(){
+   int coin=rand()%2+1;
+  cout<<coin;
+  if(coin==1){
+    cout<<"heads";
+  }
+  else {
+    cout<<"tails";
+  }
+}
 int main()
 {
- int coion
+  int t;
+  cout<<"input times to be flipped"<<endl;
+  cin>>t;
+  for(int i=0;i<t;i++){
+     int coin=rand()%2+1;
+  cout<<coin;
+  if(coin==1){
+    cout<<"heads";
+  }
+  else {
+    cout<<"tails";
+  }
+  }
+
    return 0;
 }
 
