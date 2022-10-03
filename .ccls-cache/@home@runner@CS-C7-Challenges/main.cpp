3bff946780cfca0e0faf4726e9cb6ecf277f8027
Calvin Cheam <88858058+CCheam@users.noreply.github.com>
2022-10-03 18:58:16 +0000

    start on 9

--- .ccls-cache/@home@runner@CS-C7-Challenges/main.cpp
+++ .ccls-cache/@home@runner@CS-C7-Challenges/main.cpp
@@ -1,7 +1,7 @@
 #include <iomanip>
 #include <iostream>
 #include<cstdio>
-#include <cstring>
+#include 
 int main() {
 char ansArray[20]={"B","D","A","A","C","A","B","A","C","D","B","C","D","A","D","C","C","B","D","A"};
   return 0;
