076650ba98918811c70837dc260d6fc9c8460c57
Calvin Cheam <88858058+CCheam@users.noreply.github.com>
2022-10-03 19:09:08 +0000

    progress on 9

--- .ccls-cache/@home@runner@CS-C7-Challenges/main.cpp
+++ .ccls-cache/@home@runner@CS-C7-Challenges/main.cpp
@@ -1,8 +1,12 @@
 #include <iomanip>
 #include <iostream>
 #include<cstdio>
-#include 
+#include <cstring>
 int main() {
-char ansArray[20]={"B","D","A","A","C","A","B","A","C","D","B","C","D","A","D","C","C","B","D","A"};
+char ansArray[]={'B','D','A','A','C','A','B','A','C','D','B','C','D','A','D','C','C','B','D','A'};
+char ansInp[20];
+  for(int i=0;i<20;i++){
+    
+  }
   return 0;
 }
